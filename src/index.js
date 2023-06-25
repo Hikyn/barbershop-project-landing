2784e06b4c4cdcf9934b94b9ac9a4ce6f0df2ec5
Danila <danila10danila@mail.ru>
2023-06-25 14:31:50 +0300

    styling for fullscreen welcome

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.scss";
 require('file-loader?name=[name].[ext]!./index.html')
 import mainImage from './images/barbershop.png';
+import mainLogo from './images/main-logo.png';
 import facebookIcon from './images/facebookIcon.svg';
 import instagramIcon from './images/instagramIcon.svg';
 
