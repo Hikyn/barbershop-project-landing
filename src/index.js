847ccb3800f4bc6017ab9c5651a25fcf4abd3c07
Danila <danila10danila@mail.ru>
2023-06-25 15:30:48 +0300

    add shop&map section

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./styles.scss";
 require('file-loader?name=[name].[ext]!./index.html')
 import mainImage from './images/barbershop.png';
 import barber1 from './images/barber-1.png';
+import map1 from './images/map1.png';
+import barbershopFront from './images/barbershop-front.png';
 import mainLogo from './images/main-logo.png';
 import facebookIcon from './images/facebookIcon.svg';
 import instagramIcon from './images/instagramIcon.svg';
