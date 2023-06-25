fb55476cb7d36e2cf0505f010b2269e8551cfab2
Danila <danila10danila@mail.ru>
2023-06-25 15:01:07 +0300

    add description

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.scss";
 require('file-loader?name=[name].[ext]!./index.html')
 import mainImage from './images/barbershop.png';
+import barber1 from './images/barber-1.png';
 import mainLogo from './images/main-logo.png';
 import facebookIcon from './images/facebookIcon.svg';
 import instagramIcon from './images/instagramIcon.svg';
