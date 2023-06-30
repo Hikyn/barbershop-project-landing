263aed4d5995cc8ccf084dafc956150d086207ac
Danila <danila10danila@mail.ru>
2023-06-30 12:29:21 +0300

    second store and sidebar

--- src/index.js
+++ src/index.js
@@ -5,8 +5,28 @@ import barber1 from './images/barber-1.png';
 import barber2 from './images/barber-2.png';
 import map1 from './images/map1.png';
 import barbershopFront from './images/barbershop-front.png';
+import barbershopFront2 from './images/barbershop-front2.png';
 import mainLogo from './images/main-logo.png';
+import scissors from './images/barbershop-scissors.png';
 import facebookIcon from './images/facebookIcon.svg';
 import instagramIcon from './images/instagramIcon.svg';
 
-console.log("hello world!");
+
+const button = document.querySelector('.sidebar-button');
+const sidebar = document.querySelector('.sidebar');
+
+button.addEventListener('click', () => {
+    sidebar.classList.toggle('view');
+})
+
+const btnClose = document.querySelector('.btn-close');
+btnClose.addEventListener('click', () => {
+    sidebar.classList.toggle('view');
+})
+
+const otherButtons = document.querySelectorAll('.other-btns button');
+otherButtons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        sidebar.classList.toggle('view');
+    })
+})
