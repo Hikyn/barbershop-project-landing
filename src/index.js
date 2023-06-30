fa2c16c12e31d3663b4241536507af56c6c3006c
Danila <danila10danila@mail.ru>
2023-06-30 10:49:22 +0300

    add pricelist

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./styles.scss";
 require('file-loader?name=[name].[ext]!./index.html')
 import mainImage from './images/barbershop.png';
 import barber1 from './images/barber-1.png';
+import barber2 from './images/barber-2.png';
 import map1 from './images/map1.png';
 import barbershopFront from './images/barbershop-front.png';
 import mainLogo from './images/main-logo.png';
